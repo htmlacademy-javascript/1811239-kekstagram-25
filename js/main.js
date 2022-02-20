39964e9cbb34a80f5c60a0941ec54673d337aeab
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-02-20 20:25:16 +0300

    Вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,35 +1,14 @@
-function multiplyByNineFifths(number) {
-  return number * (9/5);
-};
-
-function getFahrenheit(celsius) {
-  return multiplyByNineFifths(celsius) + 32;
-};
-
-getFahrenheit(15);
-
-
-if (multiplyByNineFifths.contains('')) {
-
-} else {
-
+function checkStringLength(str, len) {
+  if (str.lenght <= len) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log(Math.random())
 
-
-
-// 1)  Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
-// А также придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
-
-// Вопрос? (к сожалению вторую часть задания не понимаю. Какой результат должен быть и как написать такую функцию?)
-
-
-// имя_функции(проверяемая_строка, максимальная_длина);
-function checkString (){
-
+function getRandomNumber(min, max) {
+  returnMath.random() * (max - min) + min
 }
-
-
-// 2)  Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна.
-// Вопрос? Откуда берётся параметр "проверяемая_строка" и "максимальная_длина"? В каких единицах измеряется максимальная_длина строки?
+getRandomNumber(5, 15)
