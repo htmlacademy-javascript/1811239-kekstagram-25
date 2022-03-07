8159b4ccd4735073f9087a43c3f58c21cf81218d
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-03-07 18:14:11 +0300

    Больше деталей

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function checkStringLength(str, len) {
+  if (str.length <= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+checkStringLength('12',1);
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+getRandomNumber(5, 15);
