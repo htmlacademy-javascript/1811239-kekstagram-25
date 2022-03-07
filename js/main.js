5f0522d07208f067719bf3d93c37b27b3ebc1e87
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-03-07 18:40:45 +0300

    Больше деталей 7.03.22

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 getRandomNumber(5, 15);
+
+// let writeComment = [];
+// console.log(message);
+
+// {
+//   id: 24;
+//   avatar: 'img/avatar-1.svg';
+//   message: 'Всё отлично!';
+//   name: 'Анна';
+// }
