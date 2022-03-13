d592a3de24cb0465152be25c1221b6e981f2f1ae
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-03-13 19:57:50 +0300

    13.03.22

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const { name } = require("browser-sync");
-
 function checkStringLength(str, len) {
   if (str.length <= len) {
     return true;
@@ -10,7 +8,7 @@ function checkStringLength(str, len) {
 checkStringLength('12', 1);
 
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 getRandomNumber(5, 15);
 
@@ -19,18 +17,21 @@ const avatar = ['img/avatar-1.svg', 'img/avatar-2.svg', 'img/avatar-3.svg', 'img
 const message = ['Всё отлично!', 'В целом всё неплохо.Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !'];
 const appellation = ['Анна', 'Андрей', 'Денис', 'Игорь', 'Андрей', 'Марина', 'Сергей', 'Наталия', 'Ева', 'Светлана', 'Олеся', 'Жанна', 'Соня', 'София', 'Александра', 'Савва', 'Вася', 'Дима', 'Ульяна', 'Нина', 'Галина', 'Алла', 'Яна', 'Владимир', 'Лариса', 'Тоня',];
 
-const result = ['id', 'avatar', 'message', 'name'];
+const result = [];
+
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const generateRandomPost = () => ({
+  id: getRandomArrayElement(id),
+  avatar: getRandomArrayElement(avatar),
+  name: getRandomArrayElement(appellation),
+  message: getRandomArrayElement(message),
+
+});
+
 
 for (let i = 0; i < 25; i++) {
   result.push(generateRandomPost());
 }
-
-const generateRandomPost = () => {
-  id += 1;
-
-  return {
-    'id', 'avatar', 'message', 'appellation',
-    message: MESSAGES[getRandomInt()],
-  }
-}
-
+console.log(result);
