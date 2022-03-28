567b015d529c2b1e351862df5d0581f6d50ddf73
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-03-28 21:51:13 +0300

    28.03.22

--- js/fullscreenpic.js
+++ js/fullscreenpic.js
@@ -1,2 +1,26 @@
- conct element = document.getElementById("big-picture  overlay  hidden");
-  element.classList.remove("hidden");
+function openBigPic(Newphoto) {
+  // console.log(Newphoto);
+}
+export { openBigPic };
+
+const imgBigPicture = document.querySelector('.big-picture');
+imgBigPicture.classList.remove('hidden');
+// imgBigPicture.querySelector('.big-picture').classList.remove('hidden');
+
+const foundLikes =document.querySelector('likes-count');
+foundLikes.textContent = Newphoto.foundLikes;
+
+const foundComments =document.querySelector('social__comment');
+foundComments.textContent = Newphoto.foundComments;
+
+// similarWizards.forEach((wizard) => {
+//   const wizardElement = similarWizardTemplate.cloneNode(true);
+//   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+//   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+//   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+//   similarListElement.appendChild(wizardElement);
+// });
+// const similarListElement = imgBigPicture.querySelector('.paintpic-similar-list');
+// const similarimgBigPicture = document.querySelector('#similar-picture-section')
+//   .content
+//   .querySelector('.paintpic-similar-item');
