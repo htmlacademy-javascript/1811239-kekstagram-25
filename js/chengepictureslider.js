3689a5974e60e6cbedf048e640f38cf05dca2674
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-04-12 19:16:59 +0300

    12.04.22

--- js/chengepictureslider.js
+++ js/chengepictureslider.js
@@ -1,15 +1,18 @@
+const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
+const allEffects = document.querySelectorAll('.effects__radio');
 function changePictureSize(item) {
   //const pictureSize = 100;
   const sizeValue = document.querySelector('.scale__control--value');
   if (item.target.textContent === 'Уменьшить') {
-    const scaleValue = parseInt(sizeValue.value) - 25;
+    const scaleValue = Number(sizeValue.value) - 25;
     if (scaleValue >= 25) {
       sizeValue.value = scaleValue;
       document.querySelector('.img-upload__preview img').style.transform = `scale(${scaleValue / 100})`;
 
     }
   } else {
-    const scaleValue = parseInt(sizeValue.value) + 25;
+    const scaleValue = Number(sizeValue.value) + 25;
     if (scaleValue <= 100) {
       sizeValue.value = scaleValue;
       document.querySelector('.img-upload__preview img').style.transform = `scale(${scaleValue / 100})`;
@@ -26,31 +29,83 @@ buttonDoBigger.addEventListener('click', (event) => {
   changePictureSize(event);
 });
 
-//var t = document.querySelector('.img-upload__preview').children[0];
-//t.classList.add('effects__preview--chrome');
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  const newValue = sliderElement.noUiSlider.get();
+  valueElement.value = newValue;
+});
+
+allEffects.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (event.target.value === 'chrome'){
+      sliderElement.noUiSlider.updateOptions({range: {
+        min: 0,
+        max: 100,
+      },
+      start: 80,
+      format: {
+        to: function (value) {
+          if (Number.isInteger(value)) {
+            return value.toFixed(0);
+          }
+          return value.toFixed(1);
+        },
+        from: function (value) {
+          return parseFloat(value);
+        },
+      },});
+    }
+  }
+  );
+});
+
 const efectButton = document.querySelectorAll('.effects__item');
 efectButton.forEach((item) => {
   item.addEventListener('click', (event) => {
     //console.log(event);
     switch (event.target.textContent) {
       case 'Превью фото без эффекта':
-        document.querySelector('.img-upload__preview').children[0].classList.add = ('effects__preview--none');
+        document.querySelector('.img-upload__preview').children[0].classList = [];
+        document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--none');
         break;
       case 'Превью эффекта Хром':
+        document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--chrome');
         // document.querySelector('.img-upload__preview').children[0].style.filter = 'effects__preview--chrome';
         break;
       case 'Превью эффекта Сепия':
+        document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--sepia');
         // alert('Сепия');
         break;
       case 'Превью эффекта Марвин':
+        document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--marvin');
         break;
       case 'Превью эффекта Фобос':
+        document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--phobos');
         break;
       case 'Превью эффекта Зной':
+        document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--heat');
         break;
     }
