85b623ce9d55eb38a2ed86e24236da51589484c4
Nataliiy28 <77741570+Nataliiy28@users.noreply.github.com>
2022-04-13 16:06:58 +0300

    13.04.22 Слайдер

--- js/chengepictureslider.js
+++ js/chengepictureslider.js
@@ -1,9 +1,8 @@
 const sliderElement = document.querySelector('.effect-level__slider');
 const valueElement = document.querySelector('.effect-level__value');
-let currentEffect='';
-const allEffects = document.querySelectorAll('.effects__radio');
+let currentEffect = '';
+// const allEffects = document.querySelectorAll('.effects__radio');
 function changePictureSize(item) {
-  //const pictureSize = 100;
   const sizeValue = document.querySelector('.scale__control--value');
   if (item.target.textContent === 'Уменьшить') {
     const scaleValue = Number(sizeValue.value) - 25;
@@ -20,7 +19,7 @@ function changePictureSize(item) {
     }
   }
 }
-// var num = parseInt("071", 8);
+
 const buttonDoSmall = document.querySelector('.scale__control--smaller');
 const buttonDoBigger = document.querySelector('.scale__control--bigger');
 buttonDoSmall.addEventListener('click', (event) => {
@@ -52,36 +51,48 @@ noUiSlider.create(sliderElement, {
 sliderElement.noUiSlider.on('update', () => {
   const newValue = sliderElement.noUiSlider.get();
   valueElement.value = newValue;
-  if( currentEffect==='chrome'){
-    document.querySelector('.img-upload__preview').children[0].style.filter=`grayscale(${newValue})`;
-
+  if (currentEffect === 'original') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `(${newValue})`;
   }
+  if (currentEffect === 'chrome') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `grayscale(${newValue})`;
+  }
+  if (currentEffect === 'sepia') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `sepia(${newValue})`;
+  }
+  if (currentEffect === 'invert') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `invert(${newValue}%)`;
+  }
+  if (currentEffect === 'blur') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `blur(${newValue}px)`;
+  }
+  if (currentEffect === 'brightness') {
+    document.querySelector('.img-upload__preview').children[0].style.filter = `brightness(${newValue})`;
+  }
+
 });
 
-allEffects.forEach((item) => {
-  item.addEventListener('click', (event) => {
-    if (event.target.value === 'chrome'){
-      sliderElement.noUiSlider.updateOptions({range: {
-        min: 0,
-        max: 100,
+const setSliderOptions = function (from, to, step) {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: from,
+      max: to,
+    },
+    start: 0,
+    step: step,
+    format: {
+      to: function (value) {
+        if (Number.isInteger(value)) {
+          return value.toFixed(0);
+        }
+        return value.toFixed(1);
       },
-      start: 80,
-      format: {
-        to: function (value) {
-          if (Number.isInteger(value)) {
-            return value.toFixed(0);
-          }
-          return value.toFixed(1);
-        },
-        from: function (value) {
-          return parseFloat(value);
-        },
-      },});
-    }
-  }
-  );
-});
-
+      from: function (value) {
+        return parseFloat(value);
+      },
+    },
+  });
+};
 const efectButton = document.querySelectorAll('.effects__item');
 efectButton.forEach((item) => {
   item.addEventListener('click', (event) => {
@@ -90,29 +101,38 @@ efectButton.forEach((item) => {
       case 'Превью фото без эффекта':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--none');
+        currentEffect = 'none';
         break;
       case 'Превью эффекта Хром':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--chrome');
-        currentEffect='chrome';
+        setSliderOptions(0, 1, 0.1);
+        currentEffect = 'chrome';
         // document.querySelector('.img-upload__preview').children[0].style.filter = 'effects__preview--chrome';
         break;
       case 'Превью эффекта Сепия':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--sepia');
-        // alert('Сепия');
+        setSliderOptions(0, 1, 0.1);
+        currentEffect = 'sepia';
         break;
       case 'Превью эффекта Марвин':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--marvin');
+        setSliderOptions(0, 100, 1);
+        currentEffect = 'invert';
         break;
       case 'Превью эффекта Фобос':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--phobos');
+        setSliderOptions(0, 3, 0.1);
+        currentEffect = 'blur';
         break;
       case 'Превью эффекта Зной':
         document.querySelector('.img-upload__preview').children[0].classList = [];
         document.querySelector('.img-upload__preview').children[0].classList.add('effects__preview--heat');
+        setSliderOptions(1, 3, 0.1);
+        currentEffect = 'brightness';
         break;
     }
   });
